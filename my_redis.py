d297b4064d5419fd40b69d7e250658088521ab25
Oran Ben-Zur <oran@broadmann.com>
2020-10-24 16:08:13 +0300

    done

---
+++ my_redis.py
@@ -0,0 +1,42 @@
+import redis
+
+# NOTE: I use a weak single redis cluster locally with bad performance due to networking container.
+# I choose to do it only beacuse that way I can control all testing envrionments instead of
+# instructing to install redis (with better performance) and pay for it in bugs per OS and redis
+# versions. In real life I'll use muitiple redis clusters managed by redislabs on AWS - from past
+# experience if I had that opportonity I could just write every word with pipeline directly to
+# redis and it whould have been much faster and simpler than creating an in memory logic to count
+#  words and save to redis when a condition is met.
+
+class Redis:
+    """
+    This class handles redis connection and IO.
+    """
+    redis_client = redis.Redis(host='redis', port=6379)
+
+    def save(self, counter):
+        """
+        This method saves data into redis and clears the counter so the data will not be added
+        again as duplicated data.
+
+        @type counter: collections.Counter
+        @param counter: dict => {word: num_of_appearance}
+        """
+        # TODO: need to add try except and retries in case of connection issues
+        with self.redis_client.pipeline() as pipe:
+            for key, value in counter.items():
+                pipe.incrby(key, value)
+            pipe.execute()
+        counter.clear()
+
+    def get(self, key):
+        """
+        This method gets a key from redis as return it's value as int.
+
+        @type key: str
+        @param key: key=word to search.
+        @rtype: int
+        @return: word search appearance
+        """
+        # Assumption: if a key is missing we will return 0
+        return int(self.redis_client.get(key) or 0)
